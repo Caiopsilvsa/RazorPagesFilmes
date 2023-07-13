3098f8461b710891cf2e1c4a2d1ef2f82fb6b35d
caiopsilvsa <caio.psilva@outlook.com>
2023-07-13 12:49:21 -0300

    Primeiro commit

---
+++ RazorPagesFilmes/Models/Filme.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesFilmes.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [StringLength(30)]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [Display(Name = "Gênero")]
+        [StringLength (30)]
+        public string Genero { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType (DataType.Currency)]
+        public decimal Preco { get; set; }
+
+
+    }
+}
---
+++ RazorPagesFilmes/Pages/Filmes/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilmes.Data;
+using RazorPagesFilmes.Models;
+
+namespace RazorPagesFilmes.Pages.Filmes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesFilmes.Data.RazorPagesFilmesContext _context;
+
+        public IndexModel(RazorPagesFilmes.Data.RazorPagesFilmesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Filme> Filme { get;set; } = default!;
+
+        [BindProperty(SupportsGet =true)]
+        public string FilmeDigitado { get; set; }
+
+        [BindProperty(SupportsGet =true)]
+        public string GeneroSelecionado { get; set; }
+        
+        public SelectList Generos;
+
+        public async Task OnGetAsync()
+        {
+            var filmes = from f in _context.Filme
+                         select f;
+
+            if (!string.IsNullOrWhiteSpace(GeneroSelecionado))
+            {
+                filmes = filmes.Where(f => f.Genero == GeneroSelecionado);
+            }
+
+            if (!string.IsNullOrWhiteSpace(FilmeDigitado))
+            {
+                filmes = filmes.Where(f => f.Titulo.Contains(FilmeDigitado));
+            }
+
+            Generos = new SelectList(await _context.Filme.Select(f => f.Genero).Distinct().ToListAsync());
+            Filme = await filmes.ToListAsync(); 
+        }
+    }
+}
